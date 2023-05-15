e2648fe1b36a517c9bde0f30ecdacd9867e25f80
Djibril <djibriltamou@gmail.com>
2023-05-15 18:00:44 +0100

    ajout

---
+++ db/panier.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 15 mai 2023 à 17:56
+-- Version du serveur : 8.0.33-0ubuntu0.22.04.1
+-- Version de PHP : 8.1.2-1ubuntu2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `panier`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `price` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `img`, `price`, `name`) VALUES
+(1, 'i1.jpeg', 15, 'Pommade women'),
+(2, 'i2.jpeg', 20, 'Pommade baby'),
+(3, 'i3.jpeg', 14, 'Pammade'),
+(4, 'i4.jpeg', 25, 'Girls Pommade'),
+(5, 'i5.jpeg', 10, 'Pammade Men'),
+(6, 'i6.jpeg', 20, 'Girls & Baby Pommade');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
